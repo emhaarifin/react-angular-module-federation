38df1976ec96dad166b5d6a9340593132a2c2508
alex-vukov <alexvukov@gmail.com>
2023-06-06 19:05:48 +0300

    Update shell react version

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,22 +1,24 @@
 import React from "react";
 import RemoteReactApp from "./RemoteReactApp";
 import RemoteAngularApp from "./RemoteAngularApp";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <h1>Host Application - React Version {React.version}</h1>
-      <h2>App 1</h2>
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <Link to="/react-app">Show Remote React App</Link>
-        <Link to="/angular-app">Show Remote Angular App</Link>
+    <BrowserRouter>
+      <div>
+        <h1>Host Application - React Version {React.version}</h1>
+        <h2>App 1</h2>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <Link to="/react-app">Show Remote React App</Link>
+          <Link to="/angular-app">Show Remote Angular App</Link>
+        </div>
+        <Routes>
+          <Route path="/react-app" element={<RemoteReactApp />} />
+          <Route path="/angular-app" element={<RemoteAngularApp />} />
+        </Routes>
       </div>
-      <Routes>
-        <Route path="/react-app" element={<RemoteReactApp />} />
-        <Route path="/angular-app" element={<RemoteAngularApp />} />
-      </Routes>
-    </div>
+    </BrowserRouter>
   );
 };
 
--- app1/src/bootstrap.js
+++ app1/src/bootstrap.js
@@ -1,10 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
