b19bb541038308af18dac781ac68ea1968fc9efa
emhaarifin <101muhammadarifin@gmail.com>
2024-02-02 04:26:15 +0700

    Add nested routes in App2

---
+++ app4/src/injectApp4.ts
@@ -0,0 +1,27 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import "zone.js";
+declare const require: any;
+const ngVersion = require('../package.json').dependencies['@angular/core'];
+
+export default function injectApp4() {
+  if (environment.production) {
+    enableProdMode();
+  }
+
+  (window as any).platform = (window as any).platform || {};
+  let platform = (window as any).platform[ngVersion];
+  if (!platform) {
+    platform = platformBrowserDynamic();
+    (window as any).platform[ngVersion] = platform;
+  }
+  platform.bootstrapModule(AppModule)
+    .catch((err: any) => console.error(err));
+
+  // platformBrowserDynamic()
+  //   .bootstrapModule(AppModule)
+  //   .catch((err) => console.error(err));
+}
---
+++ app4/webpack.config.js
@@ -0,0 +1,73 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+const share = mf.share;
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(path.join(__dirname, "tsconfig.json"), [
+  /* mapped paths to share */
+]);
+
+
+module.exports = {
+  output: {
+    uniqueName: 'app4',
+    publicPath: 'auto',
+    scriptType: 'text/javascript'
+  },
+  optimization: {
+    runtimeChunk: false,
+  },
+  experiments: {
+    outputModule: true,
+  },
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      //library: { type: "module" },
+
+      // For remotes (please adjust)
+      name: "app4",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./injectApp4": "./src/injectApp4.ts",
+      },
+
+      // For hosts (please adjust)
+      // remotes: {
+      //     "mfe1": "http://localhost:3000/remoteEntry.js",
+
+      // },
+
+      shared: share({
+        "@angular/core": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "auto",
+        },
+        "@angular/common": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "auto",
+        },
+        "@angular/common/http": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "auto",
+        },
+        "@angular/router": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "auto",
+        },
+
+        ...sharedMappings.getDescriptors(),
+      }),
+    }),
+    sharedMappings.getPlugin(),
+  ],
+};
